a24ffae47f7361152d4d56ef4a07a919112538e3
AleHashirama <alehashirama@gmail.com>
2024-03-06 13:06:14 +0100

    progressi sino ad ora.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <script>
 import Navbar from './components/Navbar.vue';
 import AppMain from './components/AppMain.vue';
+import {store} from './store.js';
+import axios from 'axios';
 
 export default {
   components: {
     Navbar,
     AppMain
-  }
+  },
+
+  data() {
+    return {
+      store,
+    }
+  },
+
+  created() {
+
+  axios.get
+    ('https://api.themoviedb.org/3/movie/548?api_key=03d6753f3d360fdeafb03f1042471a0a')
+    .then(res => {
+    this.store.films = res
+    console.log(res)
+  })
+},
 }
+
+
 </script>
 
 
