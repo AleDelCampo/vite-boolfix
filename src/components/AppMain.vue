5bc2082534190efb4a1425bb79e09db6004b9ae2
AleHashirama <alehashirama@gmail.com>
2024-03-07 11:35:57 +0100

    flags

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -15,7 +15,26 @@ export default {
   },
 
   methods: {
-    
+    imageFlag(flag) {
+      switch (flag) {
+        case 'en':
+          return '../public/gb.png';
+        case 'fr':
+          return '../public/mf.png';
+        case 'es':
+          return '../public/es.png';
+        case 'it':
+          return '../public/it.png';
+        case 'cn':
+          return '../public/cn.png';
+        case 'ja':
+          return '../public/jp.png';
+        case 'de':
+          return '../public/de.png';
+        default:
+          return '../public/mobile-logo.png';
+      }
+    },
     searchFilms() {
       axios.get(`https://api.themoviedb.org/3/search/movie?api_key=03d6753f3d360fdeafb03f1042471a0a&query=${this.searchTitle}`)
         .then(res => {
@@ -34,6 +53,7 @@ export default {
     <div v-for="film in store.films" :key="film.id">
       <h3>{{ film.title }}</h3>
       <h2>{{ film.original_title }}</h2>
+      <img :src="imageFlag(film.original_language)" alt="Flag" />
       <p>{{ film.overview }}</p>
       <span>{{ film.vote_average }}</span>
     </div>
