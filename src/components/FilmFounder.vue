09ba04570854cdcbd7c208e6f73cc52b0d975acd
AleHashirama <alehashirama@gmail.com>
2024-03-08 11:23:48 +0100

    Milestone 6 fatta

--- src/components/FilmFounder.vue
+++ src/components/FilmFounder.vue
@@ -3,32 +3,68 @@ import { store } from '../store.js';
 import axios from 'axios';
 
 export default {
-
   data() {
     return {
-    searchTitle: '',
-    store,
+      searchTitle: '',
+      store,
+      genresFilms: [],
+      selectedGenreFilms: '',
+      genresSeries: [],
+      selectedGenreSeries: ''
     };
+  },
+
+  created() {
+    this.getMovieGenres();
+    this.getSeriesGenres();
   },
 
   methods: {
     searchFilms() {
-        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=03d6753f3d360fdeafb03f1042471a0a&query=${this.searchTitle}`)
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=03d6753f3d360fdeafb03f1042471a0a&query=${this.searchTitle}`)
         .then(res => {
-            this.store.films = res.data.results;
+          this.store.films = res.data.results;
         })
     },
 
     searchTv() {
-        axios.get(`https://api.themoviedb.org/3/search/tv?api_key=03d6753f3d360fdeafb03f1042471a0a&query=${this.searchTitle}`)
+      axios.get(`https://api.themoviedb.org/3/search/tv?api_key=03d6753f3d360fdeafb03f1042471a0a&query=${this.searchTitle}`)
         .then(res => {
-            this.store.series = res.data.results;
+          this.store.series = res.data.results;
         })
     },
 
     searchAll() {
       this.searchFilms();
       this.searchTv();
+    },
+
+    getMovieGenres() {
+      axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=03d6753f3d360fdeafb03f1042471a0a&language=en-US`)
+        .then(res => {
+          this.genresFilms = res.data.genres;
+        })
+    },
+
+    filterFilmsByGenre(genreId) {
+      axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=03d6753f3d360fdeafb03f1042471a0a&with_genres=${genreId}`)
+        .then(res => {
+          this.store.films = res.data.results;
+        })
+    },
+
+    getSeriesGenres() {
+      axios.get(`https://api.themoviedb.org/3/genre/tv/list?api_key=03d6753f3d360fdeafb03f1042471a0a&language=en-US`)
+        .then(res => {
+          this.genresSeries = res.data.genres;
+        })
+    },
+
+    filterSeriesByGenre(genreId) {
+      axios.get(`https://api.themoviedb.org/3/discover/tv?api_key=03d6753f3d360fdeafb03f1042471a0a&with_genres=${genreId}`)
+        .then(res => {
+          this.store.series = res.data.results;
+        })
     }
   }
 };
@@ -38,8 +74,21 @@ export default {
 <template>
 
   <div id="search-bar">
+
     <input type="search" v-model="searchTitle" placeholder="Search a movie or series">
+
+    <select v-model="selectedGenreFilms" @change="filterFilmsByGenre(selectedGenreFilms)">
+      <option value="">Select a genre for films</option>
+      <option v-for="genreFilms in genresFilms" :value="genreFilms.id" :key="genreFilms.id">{{ genreFilms.name }}</option>
+    </select>
+
+    <select v-model="selectedGenreSeries" @change="filterSeriesByGenre(selectedGenreSeries)">
+      <option value="">Select a genre for series</option>
+      <option v-for="genreSeries in genresSeries" :value="genreSeries.id" :key="genreSeries.id">{{ genreSeries.name }}</option>
+    </select>
+
     <button class="btn" @click="searchAll"><i class="fa-solid fa-magnifying-glass"></i></button>
+    
   </div>
 
 </template>
